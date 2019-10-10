a6ef0a1b1c702fd8c11d769f9048ca1b989c84c6
Ethan Marrs <ethan@offerzen.com>
2019-10-10 13:45:59 +0200

    Fix spec whitespace

--- spec/controllers/commands_controller_spec.rb
+++ spec/controllers/commands_controller_spec.rb
@@ -1,26 +1,23 @@
-
 require 'rails_helper'
 
 RSpec.describe IntegrationTestKit::CommandsController do
   routes { IntegrationTestKit::Engine.routes }
-  
+
   describe 'POST #create' do
-    
     before do
       allow(IntegrationTestKit).to receive(:run_command)
     end
-    
+
     it 'calls the run command method correctly' do
       post :create, params: { name: 'example' }
-      
+
       expect(IntegrationTestKit).to have_received(:run_command).with('example')
     end
-    
+
     it 'responds with success' do
       post :create, params: { name: 'example' }
-      
+
       expect(response).to have_http_status(200)
     end
   end
 end
-
